b3f0893d7577b90e16c6437915edf6cbcb3de60b
koder <ichhoa129@gmail.com>
2022-03-29 08:47:06 +0700

    feat: add game management

--- chessAi/myapp/views.py
+++ chessAi/myapp/views.py
@@ -1,4 +1,6 @@
+from tokenize import String
 from django.shortcuts import render
+from .services.game import Game
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -9,6 +11,36 @@ def index(request):
 @api_view(['POST'])
 def move(request):
     data = request.data
-    print(data)
-    # get machine move
-    return Response({'move': 'e4'})
+    try:
+        game = Game()
+        res = game.move(data['data'])
+        # get machine move
+        return Response({
+            'status': 'success',
+            'data': res.uci()
+        })
+    except Exception as e:
+        return Response({'error': str(e)})
+    
+@api_view(['POST'])
+def start(request):
+    game = Game()
+    res = game.start()
+    if res:
+        return Response({'status': 'ok'})
+    else:
+        return Response({
+            'status': 'error', 
+        'message': 'Game is already started'})
+
+
+@api_view(['POST'])
+def stop(request):
+    game = Game()
+    res = game.stop()
+    if res:
+        return Response({'status': 'ok'})
+    else:
+        return Response({
+            'status': 'error', 
+        'message': 'Game is already stopped'})
