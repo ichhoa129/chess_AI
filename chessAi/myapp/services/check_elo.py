cc63141c9b1883bc171675f3508746dd8adb1b4a
Huynh Thi Khanh Linh <bongudth@users.noreply.github.com>
2022-06-23 20:35:40 +0700

    improve lichess elo

--- chessAi/myapp/services/check_elo.py
+++ chessAi/myapp/services/check_elo.py
@@ -2,6 +2,7 @@ from asyncio.log import logger
 import chess
 import lichess.api
 from time import sleep
+import requests
 
 import sys
 sys.path.append('C:/project/chess_AI/chessAi/')
@@ -22,7 +23,6 @@ class LichessElo():
                 self.params.copy(),
             )
         if game_now_play := game.get('nowPlaying'):
-            print(game_now_play[-1])
             return game_now_play[-1]
         return None
 
@@ -42,10 +42,11 @@ class LichessElo():
 
 def main():
     lichess = LichessElo()
-    game = Game()
-    game.start()
+    # game = Game()
+    # game.start()
     fen = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
-    game_id = 'LLAuvxu9'
+    game_id = '3KiV8n8P'
+    botFen = fen
     while True:
         lichess_game = lichess.get_game_info()
         sleep(1)
@@ -53,21 +54,26 @@ def main():
             sleep(2)
             lichess_game = lichess.get_game_info()
         lastMove = lichess_game.get('lastMove')
-        my_bot_color = lichess_game.get('color') # white or black
+        # my_bot_color = lichess_game.get('color') # white or black
         logger.debug('lastMove: ', lastMove)
-        board = chess.Board(fen)
-        move = game.move2(board, {
-            'from': lastMove[:2],
-            'to': lastMove[2:4],
-            'promotion': 'q',
-            'flags': 'n', 
-            'color': 'b' if my_bot_color == 'w' else 'w',
-            'piece': 'p'
-        })
-        uci_move = move.get('move').get('from') + move.get('move').get('to')
+        # board = chess.Board(fen)
+        url = 'https://pbl.howtoquamon.tk/api/move'
+        data = {
+            "move": {
+                "from": lastMove[:2],
+                "to": lastMove[2:4]
+            },
+            "fen": botFen
+        }
+        print('lichess: ', data)
+        x = requests.post(url, json = data)
+        res = x.json()
+        botFen = res.get('fen')
+        uci_move = res.get('move').get('from') + res.get('move').get('to')
+        print('my_bot: ', uci_move)
         lichess.move(game_id, uci_move)
-        fen = board.fen()
-        logger.debug('board', fen)
+        # fen = board.fen()
+        # logger.debug('board', fen)
 
 
 def main2():
